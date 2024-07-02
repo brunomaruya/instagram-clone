d6ffa6e7d32b85c7b60105f43be6f6df7b30e067
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-02 22:23:12 +0900

    update SidebarBtn

--- src/common/InstagramLogo.tsx
+++ src/common/InstagramLogo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function InstagramLogo() {
-  return <div className="logo">Instagram</div>;
+  return <div className="logo p-3">Instagram</div>;
 }
--- src/common/SidebarBtn.tsx
+++ src/common/SidebarBtn.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 export default function SidebarBtn() {
   return (
-    <div className="flex items-center gap-2 text-xl p-3 hover:bg-[#1a1a1a] rounded-lg hover:cursor-pointer">
-      <div className="size-8">
+    <div className="flex items-center gap-3 text-xl p-3 hover:bg-[#1a1a1a] rounded-lg hover:cursor-pointer">
+      <div className="size-7">
         <HomeIcon />
       </div>
-      <span className=" ">Pagina inicial</span>
+      <span className="text-base">Pagina inicial</span>
     </div>
   );
 }
