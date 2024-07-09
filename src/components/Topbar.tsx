65c286869010df8d2cc7e8e3fcdb189a4511b637
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-10 00:15:44 +0900

    update topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,9 +1,14 @@
+import { HeartIcon, PlusCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 export default function Topbar() {
   return (
-    <div className="flex items-center px-4 h-[60px]  border-b-[1px] border-gray-500">
+    <div className="flex items-center justify-between px-4 h-[60px]  border-b-[1px] border-gray-500">
       <div className="logo  p-0">Instagram</div>
+      <div className="flex space-x-2">
+        <PlusCircleIcon className="h-7 w-7" />
+        <HeartIcon className="h-7 w-7" />
+      </div>
     </div>
   );
 }
