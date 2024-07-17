888b3b050be9450f88fdda47bab45f6efec18e9d
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-17 18:37:08 +0900

    create firebase.js

---
+++ src/app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfc5k-3gFleIAOPAyUOJ2n8uP2UFf9kTE",
+  authDomain: "instagram-clone-c82c7.firebaseapp.com",
+  projectId: "instagram-clone-c82c7",
+  storageBucket: "instagram-clone-c82c7.appspot.com",
+  messagingSenderId: "154115422504",
+  appId: "1:154115422504:web:04490c39ddf4c44429f81d",
+  measurementId: "G-DJ3P4QKEM7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
