bc74a826275849be7507b294c7bef5f6f19bded0
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-30 21:14:31 +0900

    update User component

--- src/common/User.tsx
+++ src/common/User.tsx
@@ -22,40 +22,50 @@ export default function User({
 
   useEffect(() => {
     if (currentUser) {
-      setIsFollowing(currentUser.following.includes(name.toString()));
+      if (currentUser.hasOwnProperty("following")) {
+        setIsFollowing(currentUser.following.includes(name.toString()));
+      }
     }
   }, [currentUser]);
 
   return (
-    <header className="flex items-center gap-2  ">
-      <Image
-        width={500}
-        height={500}
-        src={image}
-        alt="forest"
-        className={` ${
-          type == "suggestion" ? "h-12 w-12" : "h-10 w-10"
-        } rounded-full cursor-pointer`}
-      />
-      <div className="flex-1 cursor-pointer">
-        <span onClick={() => (window.location.href = `/${name}`)}>{name}</span>{" "}
-        {type == "post" && <span> • {date ? getDateDiff(date) : ""}</span>}
-      </div>
-      <div className="cursor-pointer">
-        {type == "post" ? (
-          <EllipsisHorizontalIcon className="h-8 w-8 cursor-pointer" />
-        ) : (
-          <span
-            className="text-[#0072BD] text-xs"
-            onClick={() => {
-              follow(currentUser.username, name);
-              setIsFollowing(true);
-            }}
-          >
-            {isFollowing ? "" : "Follow"}
-          </span>
-        )}
-      </div>
-    </header>
+    <>
+      {currentUser ? (
+        <header className="flex items-center gap-2  ">
+          <Image
+            width={500}
+            height={500}
+            src={image}
+            alt="forest"
+            className={` ${
+              type == "suggestion" ? "h-12 w-12" : "h-10 w-10"
+            } rounded-full cursor-pointer`}
+          />
+          <div className="flex-1 cursor-pointer">
+            <span onClick={() => (window.location.href = `/${name}`)}>
+              {name}
+            </span>{" "}
+            {type == "post" && <span> • {date ? getDateDiff(date) : ""}</span>}
+          </div>
+          <div className="cursor-pointer">
+            {type == "post" ? (
+              <EllipsisHorizontalIcon className="h-8 w-8 cursor-pointer" />
+            ) : (
+              <span
+                className="text-[#0072BD] text-xs"
+                onClick={() => {
+                  follow(currentUser.username, name);
+                  setIsFollowing(true);
+                }}
+              >
+                {isFollowing || currentUser.username === name ? "" : "Follow"}
+              </span>
+            )}
+          </div>
+        </header>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
