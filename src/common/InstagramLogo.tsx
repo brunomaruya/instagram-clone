33506fd024f26c647bc7baff98fd0d752574e244
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-30 21:16:48 +0900

    update logo

--- src/common/InstagramLogo.tsx
+++ src/common/InstagramLogo.tsx
@@ -1,9 +1,15 @@
+"use client";
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
 
 export default function InstagramLogo() {
   return (
-    <div>
+    <div
+      className="cursor-pointer"
+      onClick={() => {
+        window.location.href = "/";
+      }}
+    >
       <div className="logo p-3 hidden 2xl:block 2xl:text-3xl ">Instagram</div>
       <div className="logo  p-3 2xl:hidden md:text-2xl">
         <FaInstagram />
