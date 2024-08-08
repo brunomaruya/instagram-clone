d7a82079d69c372d3b56586294a6dd8f81616b94
bruno maruya <brun0yuj1ym02@gmail.com>
2024-08-08 16:29:06 +0900

    update chat

--- src/app/(root)/direct/Chat.tsx
+++ src/app/(root)/direct/Chat.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode, useContext, useState } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 import userImg from "../../../../public/assets/user.jpg";
 import Image from "next/image";
 import {
@@ -8,6 +8,20 @@ import {
   VideoCameraIcon,
 } from "@heroicons/react/24/outline";
 import { DataContext } from "@/contexts/DataContext";
+import {
+  addDoc,
+  and,
+  collection,
+  doc,
+  getDocs,
+  or,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "@/app/firebase";
+import { getDateDiff } from "@/functions/getDateDiff";
+import { toDateTime } from "@/functions/toDateTime";
 
 function Header({
   user,
@@ -24,6 +38,7 @@ function Header({
           height={500}
           className="w-14 h-14 rounded-full"
         />
+
         <div className="font-bold">{user.username}</div>
       </div>
       <div className="flex">
@@ -44,12 +59,14 @@ function Header({
 function Message({
   sentUser,
   text,
+  date,
 }: {
   sentUser: "me" | "friend";
   text: string;
+  date: any;
 }) {
   return (
-    <div className={`w-full mb-2`}>
+    <div title={toDateTime(date.seconds).toString()} className={`w-full mb-2`}>
       <div
         className={`flex gap-2 items-center  mx-4  ${
           sentUser === "me" ? "justify-end" : ""
@@ -76,37 +93,104 @@ function Message({
 
 export default function Chat({ username }: { username: string }) {
   const { users, currentUser } = useContext(DataContext);
+  const [targetUser, setTargetUser] = useState<any>();
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<any[]>([]);
 
-  const user = () => {
-    const result = users.find(
-      (user: { username: string }) => user.username === username
+  async function sendMessage() {
+    const date = new Date();
+
+    const docRef = await addDoc(collection(db, "messages"), {
+      sentUser: currentUser.username,
+      targetUser: targetUser?.username,
+      date: date,
+      text: message,
+    });
+  }
+
+  async function getMessages() {
+    const querySnapshot = await getDocs(
+      query(
+        collection(db, "messages"),
+        or(
+          and(
+            where("targetUser", "==", currentUser?.username),
+            where("sentUser", "==", targetUser?.username)
+          ),
+          and(
+            where("targetUser", "==", targetUser?.username),
+            where("sentUser", "==", currentUser?.username)
+          )
+        )
+      )
     );
-    return result;
-  };
+
+    querySnapshot.forEach((doc) => {
+      setMessages((prev) => [...prev, doc.data()]);
+    });
+  }
+
+  useEffect(() => {
+    setTargetUser((prev: any) => {
+      const result = users.find(
+        (user: { username: string }) => user.username === username
+      );
+      return result;
+    });
+  }, [users]);
+
+  useEffect(() => {
+    if (users.length > 0) getMessages();
+  }, [targetUser]);
 
   return (
     <>
-      {user() && (
+      {targetUser && (
         <section className="w-full relative">
           <div className="fixed top-0 left-[120px]  md:left-[calc(77px+120px)]  lg:left-[calc(77px+380px)] right-0">
-            <Header user={user()} />
+            <Header user={targetUser} />
           </div>
 
-          <div className="absolute bottom-[calc(48px+76px)] md:bottom-[76px]  left-0 right-0 bg-blue-200">
-            <Message sentUser="friend" text="Hello" />
-            <Message sentUser="me" text="Hello" />
+          <div className="absolute bottom-[calc(48px+76px)] md:bottom-[76px]  left-0 right-0">
+            {messages
+              .sort((a: any, b: any) => {
+                const first: any = new Date(toDateTime(a.date.seconds));
+                const second: any = new Date(toDateTime(b.date.seconds));
+                const result = first - second;
+                return result;
+              })
+              .map((message, index) => {
+                return (
+                  <Message
+                    date={message.date}
+                    key={index}
+                    sentUser={
+                      message.sentUser === currentUser.username
+                        ? "me"
+                        : "friend"
+                    }
+                    text={message.text}
+                  />
+                );
+              })}
           </div>
           <div className="fixed bottom-12  md:bottom-0 left-[120px] md:left-[calc(77px+120px)] lg:left-[calc(77px+380px)] right-0">
             <footer className="w-full  ">
-              <form className="m-4" onSubmit={(e) => e.preventDefault()}>
+              <div className="m-4">
                 <input
                   onChange={(e) => setMessage(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      setMessage("");
+                      sendMessage();
+                    }
+                  }}
                   type="text"
                   placeholder="Message..."
                   className="bg-transparent rounded-full w-full p-4  border-[1px] border-white h-11  "
+                  value={message}
                 />
-              </form>
+              </div>
             </footer>
           </div>
         </section>
