ea04012bb9f23f9654b6bd086baf101f3afed5ac
bruno maruya <brun0yuj1ym02@gmail.com>
2024-09-09 17:49:17 +0900

    create explore page

--- src/app/(root)/explore/page.tsx
+++ src/app/(root)/explore/page.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+"use client";
+import { DataContext } from "@/contexts/DataContext";
+import Image from "next/image";
+import React, { useContext } from "react";
 
 export default function page() {
-  return <div>page</div>;
+  const { posts } = useContext(DataContext);
+  return (
+    <div className="ml-[335px] mt-10  ">
+      <div className="grid grid-cols-3 gap-2 w-[967px] mx-auto  ">
+        {posts &&
+          posts.map((post: any) => (
+            <Image
+              src={post.url}
+              width={500}
+              height={500}
+              alt="image"
+              className="object-cover w-full h-full"
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
